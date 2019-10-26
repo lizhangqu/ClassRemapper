657ff745e0b19385fcfd72b8ecd5b2871b340cc2
李樟取 <lizhangqu@weidian.com>
2019-10-26 15:46:31 +0800

    * add aar support

--- remapper/src/main/java/io/github/lizhangqu/remapper/ClassRemapper.java
+++ remapper/src/main/java/io/github/lizhangqu/remapper/ClassRemapper.java
@@ -2,6 +2,8 @@ package io.github.lizhangqu.remapper;
 
 import javassist.ClassPool;
 import javassist.CtClass;
+import org.zeroturnaround.zip.ZipUtil;
+import org.zeroturnaround.zip.commons.FileUtils;
 
 import java.io.*;
 import java.util.Enumeration;
@@ -21,6 +23,7 @@ public class ClassRemapper {
     private final String newPrefix;
     private final String originalAndroidPackageName;
     private final String newAndroidPackageName;
+    private boolean isAndroidAarFile = false;
 
     private final Map<String, String> mappedName = new HashMap<>();
 
@@ -45,6 +48,23 @@ public class ClassRemapper {
 
     private void initMappedName() throws IOException {
         ZipFile zipFile = new ZipFile(srcFile);
+
+        if (zipFile.getEntry("AndroidManifest.xml") != null) {
+            if (originalAndroidPackageName == null || newAndroidPackageName == null) {
+                throw new RuntimeException("It's Android aar zipFile, you should provide originalAndroidPackageName and newAndroidPackageName");
+            }
+            isAndroidAarFile = true;
+        }
+
+        if (isAndroidAarFile) {
+            File temp = new File(outFile.getParentFile(), "temp");
+            ZipUtil.unpack(srcFile, temp);
+            File classesJar = new File(temp, "classes.jar");
+            zipFile.close();
+            zipFile = new ZipFile(classesJar);
+            FileUtils.deleteQuietly(temp);
+        }
+
         Enumeration<? extends ZipEntry> entries = zipFile.entries();
         while (entries.hasMoreElements()) {
             ZipEntry zipEntry = entries.nextElement();
@@ -63,8 +83,46 @@ public class ClassRemapper {
         }
     }
 
-
     public boolean remap() {
+        if (isAndroidAarFile) {
+            File temp = new File(outFile.getParentFile(), "temp");
+            try {
+                ZipUtil.unpack(srcFile, temp);
+                File classesJar = new File(temp, "classes.jar");
+                File remappedJar = new File(temp, "remapped.jar");
+                boolean remap = remap(classesJar, remappedJar, mappedName);
+                if (remap) {
+                    classesJar.delete();
+                    remappedJar.renameTo(classesJar);
+                }
+                File manifestXml = new File(temp, "AndroidManifest.xml");
+                File aaptManifestXml = new File(temp, "aapt/AndroidManifest.xml");
+                String manifestXmlContent = FileUtils.readFileToString(manifestXml);
+                manifestXmlContent = manifestXmlContent.replace(originalAndroidPackageName, newAndroidPackageName);
+                FileUtils.deleteQuietly(manifestXml);
+                FileUtils.copy(new ByteArrayInputStream(manifestXmlContent.getBytes()), manifestXml);
+
+                String aaptManifestXmlContent = FileUtils.readFileToString(aaptManifestXml);
+                aaptManifestXmlContent = aaptManifestXmlContent.replace(originalAndroidPackageName, newAndroidPackageName);
+                FileUtils.deleteQuietly(aaptManifestXml);
+                FileUtils.copy(new ByteArrayInputStream(aaptManifestXmlContent.getBytes()), aaptManifestXml);
+
+                ZipUtil.pack(temp, outFile);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                FileUtils.deleteQuietly(temp);
+            }
+        } else {
+            return remap(srcFile, outFile, mappedName);
+        }
+        return false;
+    }
+
+    private static boolean remap(File srcFile, File outFile, Map<String, String> mappedName) {
+        System.out.println(srcFile);
+        System.out.println(outFile);
         FileInputStream fis = null;
         ZipInputStream zis = null;
         FileOutputStream fos = null;
@@ -89,7 +147,6 @@ public class ClassRemapper {
                     for (String key : keys) {
                         ctClass.replaceClassName(key, mappedName.get(key));
                     }
-
                     String newEntryName = ctClass.getName();
                     if (newEntryName != null && newEntryName.length() > 0) {
                         newEntryName = newEntryName.replace(".", "/");
@@ -99,8 +156,14 @@ public class ClassRemapper {
                     if (!newEntryName.endsWith(".class")) {
                         newEntryName = newEntryName + ".class";
                     }
+                    ctClass.freeze();
                     zos.putNextEntry(new ZipEntry(newEntryName));
                     zos.write(ctClass.toBytecode());
+                } else {
+                    zos.putNextEntry(new ZipEntry(entry.getName()));
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    copy(zis, byteArrayOutputStream);
+                    zos.write(byteArrayOutputStream.toByteArray());
                 }
                 entry = zis.getNextEntry();
             }
@@ -139,5 +202,19 @@ public class ClassRemapper {
         }
         return false;
     }
+
+    public static void copy(InputStream inputStream, OutputStream outputStream) {
+        int length = -1;
+        byte[] buffer = new byte[4096];
+        try {
+            while ((length = inputStream.read(buffer, 0, buffer.length)) != -1) {
+                outputStream.write(buffer, 0, length);
+                outputStream.flush();
+            }
+        } catch (Exception ignore) {
+
+        }
+    }
+
 
 }
--- remapper/src/main/java/io/github/lizhangqu/remapper/Test.java
+++ remapper/src/main/java/io/github/lizhangqu/remapper/Test.java
@@ -4,8 +4,12 @@ import java.io.File;
 
 public class Test {
     public static void main(String[] args) {
-        ClassRemapper classRemapper = new ClassRemapper(new File("working/bugly.jar"), new File("working/out.jar"), "com.tencent.bugly", "shadow.bugly");
-        boolean remap = classRemapper.remap();
-        System.out.println("result:" + remap);
+        ClassRemapper classRemapperJar = new ClassRemapper(new File("working/bugly.jar"), new File("working/out.jar"), "com.tencent.bugly", "shadow.bugly");
+        boolean remapJar = classRemapperJar.remap();
+        System.out.println("jar result:" + remapJar);
+
+        ClassRemapper classRemapperAar = new ClassRemapper(new File("/Users/lizhangqu/IdeaProjects/ClassRemapper/working/bugly.aar"), new File("/Users/lizhangqu/IdeaProjects/ClassRemapper/working/out.aar"), "com.tencent.bugly", "shadow.bugly", "com.tencent.bugly.crashreport", "shadow.bugly.crashreport");
+        boolean remapAar = classRemapperAar.remap();
+        System.out.println("aar result:" + remapAar);
     }
 }
